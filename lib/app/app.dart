ea2dbbb0aa10279cda35e124b7ceaa54cb31552a
Majd Sharabah <MajdSharabah8396@gmail.com>
2022-03-22 00:47:59 +0300

    commit for style,theme,values,font and color manager

---
+++ lib/app/app.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:untitled1/app/presentation/theme_manager.dart';
+
+class MyApp extends StatefulWidget {
+  MyApp._internal(); // private named constructor
+  int appState = 0;
+  static final MyApp instance =
+  MyApp._internal(); // single instance -- singleton
+
+  factory MyApp() => instance; // factory for the class instance
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: getApplicationTheme(),
+    );
+  }
+}
